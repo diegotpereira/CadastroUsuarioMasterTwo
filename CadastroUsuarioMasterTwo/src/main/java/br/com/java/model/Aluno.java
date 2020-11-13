06d043180b556b9fe2b1503622a5a3d1352dfe1c
Diego <diegoteixeirapereira@hotmail.com>
2020-11-13 14:13:08 -0300

    criação do arquivo classe aluno.java

---
+++ CadastroUsuarioMasterTwo/src/main/java/br/com/java/model/Aluno.java
@@ -0,0 +1,36 @@
+package br.com.java.model;
+
+import java.util.Calendar;
+
+public class Aluno {
+	
+	private int id;
+	private String nome;
+	private int idade;
+	private Calendar dataNascimento;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+}
