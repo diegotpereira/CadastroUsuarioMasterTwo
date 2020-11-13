fa7491d89b6af0ecc34ea98b401c7c1e2f1ad219
Diego <diegoteixeirapereira@hotmail.com>
2020-11-13 15:07:52 -0300

    criação do arquivo classe alunoDaoImpl.java

---
+++ CadastroUsuarioMasterTwo/src/main/java/br/com/java/dao/AlunoDaoImpl.java
@@ -0,0 +1,34 @@
+package br.com.java.dao;
+
+import java.util.List;
+
+import br.com.java.model.Aluno;
+
+public class AlunoDaoImpl {
+
+	public List<Aluno> getTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void adicionar(Aluno aluno) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void atualizar(Aluno aluno) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void remover(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Aluno achar(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
