8e8e741545ca68f229c6b189f0bd3509f1207d1a
Diegotpereira <diegoteixeirapereira@hotmail.com>
2020-11-16 17:28:17 -0300

    Alteração no arquivo classe fabricaconexoes.java modificação metodo getConnection

--- CadastroUsuarioMasterTwo/src/main/java/br/com/java/dao/FabricaConexoes.java
+++ CadastroUsuarioMasterTwo/src/main/java/br/com/java/dao/FabricaConexoes.java
@@ -7,35 +7,51 @@ import java.sql.SQLException;
 
 public class FabricaConexoes {
 	
-	String driver = "com.mysql.cj.jdbc.Driver";
-	String url = "jdbc:mysql://localhost:3306/dbcadastrousuariomastertwo?useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false";
-	String usuario = "root";
-	String senha = "root";
-	
-	private static FabricaConexoes conexao;
-	
-	private FabricaConexoes() {
-		
+	public Connection getConexao() {
+		Connection conexao = null;
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String usuario = "root";
+		String senha = "root";
+		String nomeBancoDados = "jdbc:mysql://localhost:3306/dbcadastrousuariomastertwo?useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false";
+ 
 		try {
 			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
-			// TODO: handle exception
-			throw new RuntimeException(e);
-		}
-	}
-	public Connection getConnection() throws SQLException {
-		
-		Connection CON = null;
-		CON = DriverManager.getConnection(url, usuario, senha);
-		return CON;
-		
-	}
-	public static FabricaConexoes getInstance() {
-		if (conexao == null) {
-			conexao = new FabricaConexoes();
+			conexao = DriverManager.getConnection(nomeBancoDados, usuario, senha);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return conexao;
-		
 	}
+	
+//	String driver = "com.mysql.cj.jdbc.Driver";
+//	String url = "jdbc:mysql://localhost:3306/dbcadastrousuariomastertwo?useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false";
+//	String usuario = "root";
+//	String senha = "root";
+//	
+//	private static FabricaConexoes conexao;
+//	
+//	private FabricaConexoes() {
+//		
+//		try {
+//			Class.forName(driver);
+//		} catch (ClassNotFoundException e) {
+//			// TODO: handle exception
+//			throw new RuntimeException(e);
+//		}
+//	}
+//	public Connection getConnection() throws SQLException {
+//		
+//		Connection CON = null;
+//		CON = DriverManager.getConnection(url, usuario, senha);
+//		return CON;
+//		
+//	}
+//	public static FabricaConexoes getInstance() {
+//		if (conexao == null) {
+//			conexao = new FabricaConexoes();
+//		}
+//		return conexao;
+//		
+//	}
 
 }
